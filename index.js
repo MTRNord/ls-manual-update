5e4cab87ec54e188c8c84cf132c3e72cdc9cfe26
Marcel <mtrnord1@gmail.com>
2016-11-29 14:53:07 +0100

    Add some preps

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var request = require('request');
+var jsonfile = require('jsonfile')
+var findRemoveSync = require('find-remove');
+var _ = require('lodash')
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
@@ -13,6 +17,35 @@ io.on('connection', function(socket){
 
 io.on('RupdateStatus', function(socket){
   //TODO ADD LOGIC
+  //get /repos/MTRNord/ls-vertretungsplan-desktop/releases/latest
+
+  findRemoveSync('cache.json', {age: {seconds: 3600}});)
+  if (!fs.statSync('cache.json').isFile()) {
+    request('https://api.github.com/repos/MTRNord/ls-vertretungsplan-desktop/releases/latest', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        jsonfile.writeFileSync("cache.json", body)
+      }
+    })
+    var release = jsonfile.readFileSync("cache.json")
+    var version = release["tag_name"]
+    var assets = release["assets"]
+    _.find(assets, function (key) {
+      if (assets[key]["name"] == local) {
+        var local_asset = assets[key]["name"]
+        console.log(release);
+      }
+    })
+
+  }else {
+    var release = jsonfile.readFileSync("cache.json")
+    var version = release["tag_name"]
+    var assets = release["assets"]
+  }
+
+
+
+
+
 
 
 
