50784562a72cd1ce946598cfa5ad135259ea0f4b
Marcel <mtrnord1@gmail.com>
2016-11-30 07:48:01 +0100

    fix bug

--- index.js
+++ index.js
@@ -26,28 +26,30 @@ io.on('connection', function(socket){
     } catch (e) {
 
     }
-    try {
-      fs.accessSync('cache.json', fs.F_OK);
 
-      var release = jsonfile.readFileSync("cache.json")
-      var version = release["tag_name"]
-      var assets = release["assets"]
-    } catch (e) {
-      request('https://api.github.com/repos/MTRNord/ls-vertretungsplan-desktop/releases/latest', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          jsonfile.writeFileSync("cache.json", body)
-        }
-      })
-      var release = jsonfile.readFileSync("cache.json")
-      var version = release["tag_name"]
-      var assets = release["assets"]
-      _.find(assets, function (key) {
-        if (assets[key]["name"] == local) {
-          var local_asset = assets[key]["name"]
-          console.log(release);
-        }
-      })
-    }
+    fs.access('cache.json', fs.F_OK, function(err) {
+      if (!err) {
+        var release = jsonfile.readFileSync("cache.json")
+        var version = release["tag_name"]
+        var assets = release["assets"]
+      } else {
+        request('https://api.github.com/repos/MTRNord/ls-vertretungsplan-desktop/releases/latest', function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            jsonfile.writeFileSync("cache.json", body)
+          }
+        })
+        var release = jsonfile.readFileSync("cache.json")
+        var version = release["tag_name"]
+        var assets = release["assets"]
+        _.find(assets, function (key) {
+          if (assets[key]["name"] == local) {
+            var local_asset = assets[key]["name"]
+            console.log(release);
+          }
+        })
+      }
+    });
+  
 
     io.emit('AupdateStatus', 'local');
   });
